34d7b613a977edc5d77f06ea3ffd8975fc4267ef
HadiAsiaie <h.asiayfard@gmail.com>
2013-08-11 15:01:58 +0430

    added some FAQ, and resolved some typo

--- web.js
+++ web.js
@@ -10,8 +10,8 @@ app.get('/', function(request, response) {
 app.use(express.static(__dirname + '/statics'));
 
  
-
 var port = process.env.PORT || 5000;
+port=8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
